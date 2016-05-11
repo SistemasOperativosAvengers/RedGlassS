f7b67a5a3922b360973b09a20bbd9d4052c0c665
marin <marin@BatPC>
2016-05-11 00:09:44 -0500

    Algo pasara

--- RedGlassS/src/redglasss/RedGlassS.java
+++ RedGlassS/src/redglasss/RedGlassS.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class RedGlassS {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        JOptionPane.showMessageDialog(null, "I'm Batman");
     }
     
 }
