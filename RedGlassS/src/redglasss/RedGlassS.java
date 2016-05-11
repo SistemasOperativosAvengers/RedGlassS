2d4f9cd5099ace31013bcba951732ec0c51039c8
marin <marin@BatPC>
2016-05-11 00:08:55 -0500

    Algo pasara

--- RedGlassS/src/redglasss/RedGlassS.java
+++ RedGlassS/src/redglasss/RedGlassS.java
@@ -1,10 +1,12 @@
 
 package redglasss;
 
+import javax.swing.JOptionPane;
+
 public class RedGlassS {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
     }
     
 }
